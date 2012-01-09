e1f8a0dc9113870778b8ae33fba06428bcd83cb2
Tony McCrae <tony@eelpieconsulting.co.uk>
2012-01-09 09:44:28 +0100

    Added url builder methods for place and marker search.

--- src/main/java/uk/co/eelpieconsulting/countdown/urls/CountdownApiUrlBuilder.java
+++ src/main/java/uk/co/eelpieconsulting/countdown/urls/CountdownApiUrlBuilder.java
@@ -1,5 +1,8 @@
 package uk.co.eelpieconsulting.countdown.urls;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 public class CountdownApiUrlBuilder {
 
 	private String apiHost;
@@ -10,6 +13,18 @@ public class CountdownApiUrlBuilder {
 	
 	public String getStopBoardUrl(int stopId) {
 		return apiHost + "/stopBoard/" + stopId;
+	}
+
+	public String getPlaceSearchUrl(String searchTerm) {
+		try {
+			return apiHost + "/search?searchTerm=" + URLEncoder.encode(searchTerm, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			return null;
+		}
+	}
+
+	public String getMarkerSearchUrl(double swLat, double swLng, double neLat, double neLng) {
+		return apiHost + "/markers/swLat/" + swLat + "/swLng/" + swLng + "/neLat/" + neLat + "/neLng/" + neLng + "/";
 	}
 	
 }
--- src/test/java/uk/co/eelpieconsulting/countdown/urls/CountdownApiUrlBuilderTest.java
+++ src/test/java/uk/co/eelpieconsulting/countdown/urls/CountdownApiUrlBuilderTest.java
@@ -2,17 +2,32 @@ package uk.co.eelpieconsulting.countdown.urls;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class CountdownApiUrlBuilderTest {
 
 	private static final String API_HOST = "http://countdown.tfl.gov.uk";
+	private CountdownApiUrlBuilder urlBuilder;
+	
+	@Before
+	public void setup() {
+		urlBuilder = new CountdownApiUrlBuilder(API_HOST);
+	}
 	
 	@Test
-	public void canConstructUrlForStopBoardJSONRequest() {		
-		CountdownApiUrlBuilder urlBuilder = new CountdownApiUrlBuilder(API_HOST);
-		
+	public void canConstructUrlForStopBoardJSONRequest() {				
 		assertEquals("http://countdown.tfl.gov.uk/stopBoard/53550", urlBuilder.getStopBoardUrl(53550));
+	}
+	
+	@Test
+	public void canConstructUrlForPlaceSearch() throws Exception {
+		assertEquals("http://countdown.tfl.gov.uk/search?searchTerm=Twickenham", urlBuilder.getPlaceSearchUrl("Twickenham"));
+	}
+	
+	@Test
+	public void canConstructUrlForMarkerSearch() throws Exception {
+		assertEquals("http://countdown.tfl.gov.uk/markers/swLat/51.454/swLng/-0.351/neLat/51.481/neLng/-0.307/", urlBuilder.getMarkerSearchUrl(51.454, -0.351, 51.481, -0.307));		
 	}
 
 }
