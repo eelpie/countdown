c31bd6bb501b4bee7420e90b64ddbdc9868ace1d
Tony McCrae <tony@eelpieconsulting.co.uk>
2012-01-09 12:15:45 +0100

    Added parse to read place search results.

---
+++ src/main/java/uk/co/eelpieconsulting/countdown/model/BoundingBox.java
@@ -0,0 +1,39 @@
+package uk.co.eelpieconsulting.countdown.model;
+
+public class BoundingBox {
+	
+	private final double swLat;
+	private final double swLng;
+	private final double neLat;
+	private final double neLng;
+	
+	public BoundingBox(double swLat, double swLng, double neLat, double nwLng) {
+		this.swLat = swLat;
+		this.swLng = swLng;
+		this.neLat = neLat;
+		this.neLng = nwLng;
+	}
+
+	public double getSwLat() {
+		return swLat;
+	}
+
+	public double getSwLng() {
+		return swLng;
+	}
+
+	public double getNeLat() {
+		return neLat;
+	}
+
+	public double getNeLng() {
+		return neLng;
+	}
+
+	@Override
+	public String toString() {
+		return "BoundingBox [neLat=" + neLat + ", nwLng=" + neLng + ", swLat="
+				+ swLat + ", swLng=" + swLng + "]";
+	}
+	
+}
---
+++ src/main/java/uk/co/eelpieconsulting/countdown/model/Place.java
@@ -0,0 +1,33 @@
+package uk.co.eelpieconsulting.countdown.model;
+
+public class Place {
+	
+	private final String name;
+	private final String place;
+	private final double lat;
+	private final double lng;
+	
+	public Place(String name, String place, double lat, double lng) {
+		this.name = name;
+		this.place = place;
+		this.lat = lat;
+		this.lng = lng;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getPlace() {
+		return place;
+	}
+
+	public double getLat() {
+		return lat;
+	}
+
+	public double getLng() {
+		return lng;
+	}
+	
+}
