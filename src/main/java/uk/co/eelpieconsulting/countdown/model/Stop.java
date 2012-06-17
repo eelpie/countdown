2df25d19cc1a33607f48c3dac70a71787f257554
Tony McCrae <tony@eelpieconsulting.co.uk>
2012-06-17 11:16:43 +0100

    Equals and hashset for stops is based on the id, so that they can be collected in sets.

--- src/main/java/uk/co/eelpieconsulting/countdown/model/Stop.java
+++ src/main/java/uk/co/eelpieconsulting/countdown/model/Stop.java
@@ -45,6 +45,28 @@ public class Stop implements Serializable {
 	public double getLongitude() {
 		return longitude;
 	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Stop other = (Stop) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
 
 	@Override
 	public String toString() {
