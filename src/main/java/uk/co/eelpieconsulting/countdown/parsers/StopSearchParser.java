85003152c1020678c8695d2b9dde6e2c789026e2
Tony McCrae <tony@eelpieconsulting.co.uk>
2012-07-20 13:18:36 +0100

    Refactor - label index for stop search fields.

--- src/main/java/uk/co/eelpieconsulting/countdown/parsers/StopSearchParser.java
+++ src/main/java/uk/co/eelpieconsulting/countdown/parsers/StopSearchParser.java
@@ -11,6 +11,14 @@ import uk.co.eelpieconsulting.countdown.model.Stop;
 
 public class StopSearchParser {
 
+	private static final int LONGITUDE = 7;
+	private static final int LATITUDE = 6;
+	private static final int STOP_INDICATOR = 5;
+	private static final int PUBLIC_IDENTIFIER = 2;
+	private static final int TOWARDS = 4;
+	private static final int STOP_NAME = 1;
+	private static final int STOP_ID = 3;
+	
 	private static final String NEW_LINE = "\n";
 	
 	public List<Stop> parse(String json) throws ParsingException {
@@ -23,11 +31,11 @@ public class StopSearchParser {
 			final List<Stop> stops = new ArrayList<Stop>();
 			for (int i = 1; i < lines.length; i++) {
 				final JSONArray stopJson =  new JSONArray(lines[i]);
-				if (!stopJson.isNull(3)) {
-					stops.add(new Stop(Integer.parseInt(stopJson.getString(3)),
-							stopJson.getString(1), stopJson.getString(4), stopJson.getString(2),
-							stopJson.isNull(5) ? null : stopJson.getString(5), stopJson.getDouble(6),
-							stopJson.getDouble(7)));
+				if (!stopJson.isNull(STOP_ID)) {
+					stops.add(new Stop(Integer.parseInt(stopJson.getString(STOP_ID)),
+							stopJson.getString(STOP_NAME), stopJson.getString(TOWARDS), stopJson.getString(PUBLIC_IDENTIFIER),
+							stopJson.isNull(STOP_INDICATOR) ? null : stopJson.getString(STOP_INDICATOR), stopJson.getDouble(LATITUDE),
+							stopJson.getDouble(LONGITUDE)));
 				}
 			}
 			return stops;
